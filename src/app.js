3762637d83b721731e6ec1bbd81303d3e2610ef7
Paolo Lago <paolo@recosapp.com>
2020-05-19 09:03:32 -0500

    X

--- src/app.js
+++ src/app.js
@@ -12,14 +12,6 @@ import getVisibleExpenses from './selectors/expenses'
 
 const store = configureStore()
 
-store.dispatch(addExpense({ description: 'water bill', amount: 1800 }))
-store.dispatch(addExpense({ description: 'beer bill', amount: 4200, createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'rent', amount: 104200 }))
-
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
