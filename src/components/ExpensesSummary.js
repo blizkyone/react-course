cdf1eb2ae38d91ce193ce76d9187781041339a28
Paolo Lago <paolo@recosapp.com>
2020-05-28 14:01:30 -0500

    more shit

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -2,21 +2,25 @@ import React from 'react'
 import { connect } from 'react-redux'
 import numeral from 'numeral'
 import expensesTotal from '../selectors/expenses-total'
+import filteredExpenses from '../selectors/expenses'
 
-export class ExpensesSummary extends React.Component {
-    render() {
-        return (
-            <div>
-                {this.props.expenses.length} Expenses totalling: {
-                    numeral(expensesTotal(this.props.expenses) / 100).format('$ 0,0.00')
-                }
-            </div>
-        )
-    }
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+    const expenseNoun = expenseCount === 1 ? 'expense' : 'expenses'
+    const formatedTotal = numeral(expensesTotal / 100).format('$ 0,0.00')
+    return (
+        <div>
+            {expenseCount} {expenseNoun} totalling: {formatedTotal}
+        </div>
+    )
 }
 
-const mapStateToProps = (state) => ({
-    expenses: state.expenses
-})
+const mapStateToProps = (state) => {
+    const visibleExpenses = filteredExpenses(state.expenses, state.filters)
+
+    return {
+        expenseCount: visibleExpenses.length,
+        expensesTotal: expensesTotal(visibleExpenses)
+    }
+}
 
 export default connect(mapStateToProps)(ExpensesSummary)
