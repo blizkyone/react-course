70f3cc042e8e300e4bd11f4c0684dd9a711dfb21
Paolo Lago <paolo@recosapp.com>
2020-06-02 13:14:33 -0500

    Authentication

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { startLogout } from '../actions/auth'
 
-const Header = () => (
+export const Header = ({ startLogout }) => (
     <header>
         <h1>Expensify</h1>
         <ul>
             <li><NavLink to="/" activeClassName="is-active" exact={true}>Go to Homepage</NavLink></li>
             <li><NavLink to="/create" activeClassName="is-active">Go to Add Expense page</NavLink></li>
         </ul>
+        <button onClick={startLogout}>Logout</button>
     </header>
 )
 
-export default Header
+const mapDispatchToProps = (dispatch) => ({
+    startLogout: () => dispatch(startLogout())
+})
+
+export default connect(undefined, mapDispatchToProps)(Header)
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,8 +16,9 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig)
   
 const db = firebase.database()
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
 
-export { firebase, db as default }
+export { firebase, googleAuthProvider, db as default }
 
 // db.ref('expenses').on('child_removed', (snapshot) => {
 //     console.log(snapshot.key, snapshot.val())
